68657e72eb5f82177e8946f1e2789ac019e51c47
Shilpa Dadhich <shilpadadhich52@gmail.com>
2026-03-28 18:21:07 +0530

    fix: updated backend default port to 8000

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  port: (process.env.PORT || 5000) as number,
+  port: (process.env.PORT || 8000) as number,
   jwtSecret: (process.env.JWT_SECRET || 'super-secret-key') as string,
   jwtExpiration: (process.env.JWT_EXPIRATION || '24h') as string,
   databaseUrl: process.env.DATABASE_URL as string,
